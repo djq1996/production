ef454a49e46d79e5bf7413f275f80ffa61edf0d5
董建强 <18513552894@163.com>
2016-10-26 19:25:40 +0800

    项目提交1.1
    前端董建强

---
+++ js/index.js
@@ -0,0 +1,26 @@
+function getClass(className) {
+    if (document.getElementsByClassName) {
+        return document.getElementsByClassName(className);
+    }
+    else {
+        var All = document.getElementsByTagName('*');
+        var arr = [];
+        for (var i = 0; i < All.length; i++) {
+            if (All[i].className == className) {
+                arr.push(All[i])
+            }
+ 
+ 
+        }
+        return arr;
+    }
+}
+window.onload=function(){
+	var banner=getClass('banner');
+	
+	
+	
+	
+	
+	
+}
